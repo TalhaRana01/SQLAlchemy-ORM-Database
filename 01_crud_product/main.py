27a4726b0071e2b8726961b83055f35ce64a6f45
Talha Rana <mtalharana093@gmail.com>
2025-11-25 03:32:16 +0500

    update user

--- 01_crud_product/main.py
+++ 01_crud_product/main.py
@@ -6,8 +6,14 @@ from services import *
 
 # USERS
 # create_user("hammad ali", "example2@gmail.com")
-print(Get_user_by_id(1))
+# print(Get_user_by_id(1))
+# print(get_all_user)
+
+update_user_email(1,"new_updated_eamil@gmail.com")
 
 
 # POSTS
-# create_post(1, "First post", "This is first post")
+# create_post(1, "Third post", "This is a post")
+# print(Get_post_by_id(1))
+
+# print(get_posts_by_user(1))
--- 01_crud_product/services.py
+++ 01_crud_product/services.py
@@ -1,5 +1,6 @@
 from models import User, Post
 from database import SessionLocal
+from sqlalchemy import select
 
 
 
@@ -20,11 +21,45 @@ def create_post(user_id: int, title: str, content:str):
     post = Post(user_id=user_id, title=title, content=content)
     session.add(post)
     session.commit()
-    
+
+# Get All User
+
+def  get_all_user():
+  with SessionLocal() as session:
+    stmt = select(User)
+    users = session.scalar(stmt).all()
+    return users
     
 # Get Single User
 def Get_user_by_id(user_id: int):
   with SessionLocal() as session:
     user = session.get_one(User, user_id)
     return user
+  
+  
+# Update User email
+
+def update_user_email(user_id: int, new_email: str):
+  with SessionLocal() as session:
+    user = session.get(User, user_id)
+    if user:
+      user.email = new_email
+      session.commit()
+    return user
+  
+  
+# Get Single Post
+def Get_post_by_id(post_id: int):
+  with SessionLocal() as session:
+    post = session.get_one(Post, post_id)
+    return post
+
+
+# Get all post for a user
+
+def get_posts_by_user(user_id:int):
+  with SessionLocal() as session:
+    user = session.get(User, user_id)
+    posts = user.posts if user else []
+    return posts
     
